6d9c7fface3d18b9ca759dee1143ea48e1e1a9b9
Olatunji Abass <94129113+Godswillconcept@users.noreply.github.com>
2025-05-10 16:41:27 +0100

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const mysql = require("mysql2");
 const conn = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "e-commerce",
-  port: process.env.DB_PORT || 3306,
-  uri: process.env.DB_URI || "mysql://root:@localhost:3306/e-commerce"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  uri: process.env.DB_URI
 });
 
 conn.connect((err) => console.log(err || "Database Connected"));
