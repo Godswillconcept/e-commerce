d7df4f44ef37ab0290e1af8d48598245d821575a
godswillconcept <olatunjiabass01@gmail.com>
2025-05-10 09:28:05 +0100

    Adds dotenv for environment variable management
    Configures the application to utilize environment variables for database credentials.
    
    This change enhances security and simplifies configuration management by allowing sensitive information, like database passwords, to be stored outside of the codebase.
    
    The database connection settings are now read from the environment, falling back to default values if environment variables are not set.

--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
 const mysql = require("mysql2");
 const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "e-commerce",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "e-commerce",
 });
 
 conn.connect((err) => console.log(err || "Database Connected"));
 module.exports = conn.promise();
-
