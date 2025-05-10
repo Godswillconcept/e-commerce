776687c54dccb7754abca501ef0371f69078b7bc
godswillconcept <olatunjiabass01@gmail.com>
2025-05-10 16:34:40 +0100

    Adds database connection configuration
    Configures the database connection by adding port and URI options.
    This allows for greater flexibility in database setup and connection.

--- models/connection.js
+++ models/connection.js
@@ -5,6 +5,8 @@ const conn = mysql.createConnection({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "e-commerce",
+  port: process.env.DB_PORT || 3306,
+  uri: process.env.DB_URI || "mysql://root:@localhost:3306/e-commerce"
 });
 
 conn.connect((err) => console.log(err || "Database Connected"));
